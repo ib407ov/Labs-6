d5e86c7d0e99dfa460637398100e0bacdf60d4f8
ib407ov <44349161+ib407ov@users.noreply.github.com>
2020-12-22 11:31:08 +0200

    Add files via upload

--- Завдання 2.py
+++ Завдання 2.py
@@ -1,11 +1,14 @@
-n = int(input('line lenghts: '))
+n = int(input('How many numbers = '))
+x = int(input('x = '))
+y = int(input('y = '))
+
+import random
+A = [x, x, y]
+print(A)
 
 if n < 3:
     print('Error')
     exit(0)
-
-A = []
-A = [int(input('write {0} numer : '.format(i))) for i in range(3)]
 
 for i in range(3, n):
     A.append(A[i - 2] + (A[i - 1] / (2 ** (i - 1))) * A[i - 3])
